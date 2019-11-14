92367a72d312a1f546983f75d0f1a3cbd318ab63
Manuel Troncoso <git.manuel98@gmail.com>
2019-11-14 17:08:58 +0100

    Añadida una pagina de navegación, botón e imagenes

---
+++ HSportPlus/HSportPlus/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HSportPlus
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ProductDetail());
+        }
+    }
+}
