c6e3d601ab482a84fddc42a4f0ea5a7af7ef0ae3
Manuel Troncoso <git.manuel98@gmail.com>
2019-11-15 14:14:27 +0100

    Añadido los botones

--- HSportPlus/HSportPlus/obj/Debug/netstandard2.0/ProductDetail.xaml.g.cs
+++ HSportPlus/HSportPlus/obj/Debug/netstandard2.0/ProductDetail.xaml.g.cs
@@ -17,8 +17,12 @@ namespace HSportPlus {
     public partial class ProductDetail : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Button OrderBtn;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(ProductDetail));
+            OrderBtn = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "OrderBtn");
         }
     }
 }
